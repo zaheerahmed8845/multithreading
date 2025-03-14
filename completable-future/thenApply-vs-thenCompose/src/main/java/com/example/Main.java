8d8cf33ccd871ddf0bceff200edcc107644886d3
ZAHEER AHMED <zaheerahmed8845@gmail.com>
2025-03-14 08:55:44 +0530

    Multitherading Initial Commit

---
+++ completable-future/thenApply-vs-thenCompose/src/main/java/com/example/Main.java
@@ -0,0 +1,41 @@
+package com.example;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public class Main {
+    public static void main(String[] args) {
+
+        withoutThenCompose();
+        withThenCompose();
+
+    }
+
+    private static void withoutThenCompose(){
+        CompletableFuture<CompletableFuture<String>> future =
+                CompletableFuture.supplyAsync(() -> "Hello")
+                        .thenApply(result -> CompletableFuture.supplyAsync(() -> result + " World!"));
+
+        try {
+            System.out.println("Result without then Compose: "+future.get().get());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void withThenCompose(){
+        CompletableFuture<String> future =
+                CompletableFuture.supplyAsync(() -> "Hello")
+                        .thenCompose(result -> CompletableFuture.supplyAsync(() -> result + " World!"));
+
+        try {
+            System.out.println("Result with then Compose: "+future.get());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
